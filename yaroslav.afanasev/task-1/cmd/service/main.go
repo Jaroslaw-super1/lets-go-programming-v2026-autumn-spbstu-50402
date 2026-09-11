ed82fb271e23bed39a0d7369cbb101f30d815ce8
jar <Jarafan3@gmail.com>
2026-09-11 16:41:07 +0300

    [TASK-1] make calculator

--- yaroslav.afanasev/task-1/cmd/service/main.go
+++ yaroslav.afanasev/task-1/cmd/service/main.go
@@ -3,5 +3,46 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("test")
+
+	var a, b int
+	_, err := fmt.Scan(&a)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	var op string
+	_, err = fmt.Scan(&op)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	_, err = fmt.Scan(&b)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	var res int
+	switch op {
+	case "+":
+		res = a + b
+	case "-":
+		res = a - b
+	case "*":
+		res = a * b
+	case "/":
+		if b != 0 {
+			res = a / b
+		} else {
+			fmt.Println("Division by zero")
+			return
+		}
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	fmt.Println(res)
 }
